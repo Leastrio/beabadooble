fdb0e70cef0cbc6dd16248d9e1f5e3229fc5eb91
Leastrio <jacob@leastr.io>
2024-09-26 00:34:00 -0600

    meow

--- assets/js/audio_player.js
+++ assets/js/audio_player.js
@@ -2,21 +2,29 @@ export default {
   mounted() {
     this.play_btn = document.getElementById('play');
     this.progress_bar = document.getElementById('progress');
+
     this.handleEvent("session:set_audio", ({url}) => {
       if (this.audio) {
         this.audio.pause();
         this.audio.currentTime = 0;
         if (this.animation) { this.animation.cancel() }
       }
+
       this.audio = new Audio(url)
+      this.audio.preload = "auto";
     });
+
     this.play_btn.addEventListener("click", () => {
+      if (!this.audio) return;
+
       this.audio.currentTime = 0;
+
       if (this.animation) {
         this.animation.cancel();
       }
-      this.audio.play();
-      this.animation = this.progress_bar.animate({'width': this.audio.duration / 2.5 * 100 + "%"}, this.audio.duration * 1000, 'linear');
+      this.audio.play().then(() => {
+        this.animation = this.progress_bar.animate({'width': this.audio.duration / 2.5 * 100 + "%"}, this.audio.duration * 1000, 'linear');
+      });
     });
   },
 
