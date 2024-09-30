157dca6b3e09a0a17847c09276644e82d1023df6
Leastrio <jacob@leastr.io>
2024-09-29 22:41:47 -0600

    fix audio

--- assets/js/audio_player.js
+++ assets/js/audio_player.js
@@ -3,12 +3,15 @@ export default {
     this.play_btn = document.getElementById('play-btn');
     this.progress_bar = document.getElementById('progress');
 
-    this.audios = [];
+    this.audio_buffers = [];
+    this.audio_context = new (window.AudioContext || window.webkitAudioContext)();
+    this.source = null;
 
-    this.handleEvent("session:preload_audio", ({urls, curr_idx}) => {
-      let audios_loaded = 0;
-      const total_audios = urls.length;
+    if (navigator.audioSession) {
+      navigator.audioSession.type = 'playback';
+    }
 
+    this.handleEvent("session:preload_audio", async ({urls, curr_idx}) => {
       this.curr_idx = curr_idx;
 
       document.getElementById('play-spinner').classList.remove('hidden');
@@ -16,75 +19,67 @@ export default {
       document.getElementById('play-btn').disabled = true;
       document.getElementById('skip-btn').disabled = true;
 
-      urls.forEach((url) => {
-        const audio = new Audio();
-        audio.crossOrigin = "anonymous";
-        audio.src = url;
+      const audio_promises = urls.map((url) => {
+        return new Promise((resolve, reject) => {
+          const request = new XMLHttpRequest();
+          request.open('GET', url, true);
+          request.responseType = 'arraybuffer';
 
-        audio.oncanplaythrough = function () {
-          audios_loaded++;
-
-          if (audios_loaded === total_audios) {
-            document.getElementById('play-spinner').classList.add('hidden');
-            document.getElementById('play-icon').classList.remove('hidden');
-            document.getElementById('play-btn').disabled = false;
-            document.getElementById('skip-btn').disabled = false;
+          request.onload = () => {
+            this.audio_context.decodeAudioData(request.response, (buffer) => {
+              resolve(buffer);
+            })
           }
-        };
 
-        this.audios.push(audio);
+          request.send();
+        });
       });
+
+      const buffers = await Promise.all(audio_promises);
+      this.audio_buffers = buffers.sort((a, b) => a.duration - b.duration);
+      document.getElementById('play-spinner').classList.add('hidden');
+      document.getElementById('play-icon').classList.remove('hidden');
+      document.getElementById('play-btn').disabled = false;
+      document.getElementById('skip-btn').disabled = false;
     });
 
     this.handleEvent("session:next_audio", () => {
-      const audio = this.audios[this.curr_idx];
-
-      if (audio) {
-        audio.pause();
-        audio.currentTime = 0;
+      if (this.source) {
+        this.source.stop();
         if (this.animation) { this.animation.cancel() }
-
-        if (this.audioContext) {
-          this.audioContext.close()
-          this.audioContext = null;
-        }
       }
 
       this.curr_idx++;
     });
 
-    this.play_btn.addEventListener("click", () => {
+    this.play_btn.addEventListener("click", async () => {
       if (this.play_btn.disabled) return;
 
-      const audio = this.audios[this.curr_idx];
-      audio.currentTime = 0;
+      const audio = this.audio_buffers[this.curr_idx];
 
-      if (this.audioContext) {
-        this.audioContext.close();
-      }
-
-      this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
-      const source = this.audioContext.createMediaElementSource(audio);
-      source.connect(this.audioContext.destination);
-
-      audio.onended = function() {
-        if (this.audioContext) {
-          this.audioContext.close()
-          this.audioContext = null;
+      if (this.source) {
+        this.source.stop()
+        if (this.animation) {
+          this.animation.cancel()
         }
       }
 
-      if (this.animation) {
-        this.animation.cancel();
+      this.source = this.audio_context.createBufferSource();
+      this.source.buffer = audio;
+      this.source.connect(this.audio_context.destination);
+
+      this.source.start(0)
+      this.animation = this.progress_bar.animate({'width': audio.duration / 2.5 * 100 + "%"}, audio.duration * 1000, 'linear');
+
+      this.source.onended = () => {
+        this.currentSource = null;
       }
-      audio.play().then(() => {
-        this.animation = this.progress_bar.animate({'width': audio.duration / 2.5 * 100 + "%"}, audio.duration * 1000, 'linear');
-      });
     });
   },
 
   destroyed() {
     if (this.audio) { this.audio.pause() }
     if (this.animation) { this.animation.cancel() }
+    if (this.audioContext) { this.audioContext.close() }
   }
 }
